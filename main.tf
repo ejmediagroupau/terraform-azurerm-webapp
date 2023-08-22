f8669e9ae989a09783ea733f8adf3350ddbc319c
Mikael <krief_mikael@hotmail.com>
2023-08-22 07:02:29 +0000

    update

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
 }
 
 
-resource "azurerm_service_plan" "plan-app" {
+resource "azurerm_service_plan" "plan" {
   name                = var.service_plan_name
   location            = var.location
   resource_group_name = var.resource_group_name
@@ -21,7 +21,7 @@ resource "azurerm_linux_web_app" "app" {
   name                = var.app_name
   location            = var.location
   resource_group_name = var.resource_group_name
-  service_plan_id     = azurerm_service_plan.plan-app.id
+  service_plan_id     = azurerm_service_plan.plan.id
   app_settings = {
     "INSTRUMENTATIONKEY" = azurerm_application_insights.appinsight-app.instrumentation_key
   }
