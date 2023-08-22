f45f1a13ea7fba22c3e7a2bc577684d45af7091f
Mikael <krief_mikael@hotmail.com>
2023-08-22 06:55:06 +0000

    update

--- main.tf
+++ main.tf
@@ -1,20 +1,18 @@
 
 
-resource "azurerm_app_service_plan" "plan-app" {
+resource "azurerm_service_plan" "plan-app" {
   name                = var.service_plan_name
   location            = var.location
   resource_group_name = var.resource_group_name
-  sku {
-    tier = var.sp_sku
-    size = "S1"
-  }
+  sku_name            = "S1"
+  os_type             = "Linux"
 }
 
 resource "azurerm_app_service" "app" {
   name                = var.app_name
   location            = var.location
   resource_group_name = var.resource_group_name
-  app_service_plan_id = azurerm_app_service_plan.plan-app.id
+  service_plan_id     = azurerm_service_plan.plan-app.id
   app_settings = {
     "INSTRUMENTATIONKEY" = azurerm_application_insights.appinsight-app.instrumentation_key
   }
